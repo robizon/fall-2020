06cb0da90221224911fe2894eb2ea333e1f5bd37
robizon <36480683+robizon@users.noreply.github.com>
2020-11-25 10:49:17 -0500

    j

--- ProblemSets/PS5-ddc/PS5starter.jl
+++ ProblemSets/PS5-ddc/PS5starter.jl
@@ -16,8 +16,11 @@ include("create_grids.jl")
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 # load in the data
 
+url = "https://raw.githubusercontent.com/OU-PhD-Econometrics/fall-2020/master/ProblemSets/PS5-ddc/busdataBeta0.csv"
+df = CSV.read(HTTP.get(url).body)
 
-# [Your turn]
+
+
 
 
 # create bus id variable
@@ -25,7 +28,7 @@ df = @transform(df, bus_id = 1:size(df,1))
 
 #---------------------------------------------------
 # reshape from wide to long (must do this twice be-
-# cause DataFrames.stack() requires doing it one 
+# cause DataFrames.stack() requires doing it one
 # variable at a time)
 #---------------------------------------------------
 # first reshape the decision variable
@@ -54,19 +57,22 @@ sort!(df_long,[:bus_id,:time])
 # Question 2: estimate a static version of the model
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# [Your turn]
-
+alpha_hat_glm = glm(@formula(Y ~ RouteUsage + Branded), df_long, Binomial(), LogitLink())
+println(alpha_hat_glm)
 
 
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 # Question 3a: read in data for dynamic model
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# [ Load in the data ]
+url = "https://raw.githubusercontent.com/OU-PhD-Econometrics/fall-2020/master/ProblemSets/PS5-ddc/busdata.csv"
+df = CSV.read(HTTP.get(url).body)
 
 Y = Matrix(df[:,[:Y1,:Y2,:Y3,:Y4,:Y5,:Y6,:Y7,:Y8,:Y9,:Y10,:Y11,:Y12,:Y13,:Y14,:Y15,:Y16,:Y17,:Y18,:Y19,:Y20]])
 
-# [ convert other data frame columns to matrices]
+Odo = Matrix(df[:,[:Odo1,:Odo2,:Odo3,:Odo4,:Odo5,:Odo6,:Odo7,:Odo8,:Odo9,:Odo10,:Odo11,:Odo12,:Odo13,:Odo14,:Odo15,:Odo16,:Odo17,:Odo18,:Odo19,:Odo20]])
+
+X = Matrix(df[:,[:Xst1,:Xst2,:Xst3,:Xst4,:Xst5,:Xst6,:Xst7,:Xst8,:Xst9,:Xst10,:Xst11,:Xst12,:Xst13,:Xst14,:Xst15,:Xst16,:Xst17,:Xst18,:Xst19,:Xst20]])
 
 
 
@@ -77,3 +83,9 @@ Y = Matrix(df[:,[:Y1,:Y2,:Y3,:Y4,:Y5,:Y6,:Y7,:Y8,:Y9,:Y10,:Y11,:Y12,:Y13,:Y14,:Y
 zval,zbin,xval,xbin,xtran = create_grids()
 
 
+
+#:::::::::::::::::::::::::::::::::::::::::::::::::::
+# Question 3c: Compute the future value terms
+#:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+f_v=[zeros(size(xtran, 1)), zeros(2), zeros(size(X, 2)+1)]
